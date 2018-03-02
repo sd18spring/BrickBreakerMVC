2a11289fdddc9c97a14413538adcc2b4e53d0905
Paul Ruvolo <paullundyruvolo@gmail.com>
2018-03-01 20:44:32 -0500

    add a grid of bricks and a paddle.  remove some hardcoding

--- brick_breaker.py
+++ brick_breaker.py
@@ -34,20 +34,46 @@ class PyGameWindowView(object):
                                          brick.y,
                                          brick.width,
                                          brick.height))
+        pygame.draw.rect(self.screen,
+                         pygame.Color(255, 0, 0),
+                         pygame.Rect(self.model.paddle.x,
+                                     self.model.paddle.y,
+                                     self.model.paddle.width,
+                                     self.model.paddle.height))
         pygame.display.update()
 
 class BrickBreakerModel(object):
     """ Encodes a model of the game state """
-    def __init__(self):
+    def __init__(self, size):
         self.bricks = []
-        self.bricks.append(Brick(20, 100, 10, 10))
+        self.width = size[0]
+        self.height = size[1]
+        self.brick_width = 100
+        self.brick_height = 20
+        self.brick_space = 10
+        for x in range(self.brick_space,
+                       self.width - self.brick_space - self.brick_width,
+                       self.brick_width + self.brick_space):
+            for y in range(self.brick_space,
+                           self.height//2,
+                           self.brick_height + self.brick_space):
+                self.bricks.append(Brick(self.brick_height,
+                                         self.brick_width,
+                                         x,
+                                         y))
+        self.paddle = Paddle(20, 100, 200, self.height - 30)
+
+    def update(self):
+        """ Update the game state (currently only tracking the paddle) """
+        self.paddle.update()
 
     def __str__(self):
         output_lines = []
         # convert each brick to a string for outputting
         for brick in self.bricks:
             output_lines.append(str(brick))
-        # print one brick per line
+        output_lines.append(str(self.paddle))
+        # print one item per line
         return "\n".join(output_lines)
 
 class Brick(object):
@@ -63,13 +89,33 @@ class Brick(object):
                                                           self.width,
                                                           self.x,
                                                           self.y)
+class Paddle(object):
+    """ Encodes the state of the paddle in the game """
+    def __init__(self, height, width, x, y):
+        """ Initialize a paddle with the specified height, width,
+            and position (x,y) """
+        self.height = height
+        self.width = width
+        self.x = x
+        self.y = y
+        self.vx = 0.0
+
+    def update(self):
+        """ update the state of the paddle """
+        self.x += self.vx
+
+    def __str__(self):
+        return "Paddle height=%f, width=%f, x=%f, y=%f" % (self.height,
+                                                           self.width,
+                                                           self.x,
+                                                           self.y)
 
 if __name__ == '__main__':
     pygame.init()
 
     size = (640, 480)
 
-    model = BrickBreakerModel()
+    model = BrickBreakerModel(size)
     print(model)
     view = PyGameWindowView(model, size)
 
@@ -78,6 +124,7 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
+        model.update()
         view.draw()
         time.sleep(.001)
 
